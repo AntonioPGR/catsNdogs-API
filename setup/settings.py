65b5e3a8866ff8703f910bb53fdc497386343f0d
Antonio Pacheco Gerim Ribeiro <antoninhopgr@gmail.com>
2023-04-17 22:45:51 -0300

    Response Formats

--- setup/settings.py
+++ setup/settings.py
@@ -166,7 +166,17 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_THROTTLE_RATES': {
         'anon': '100/day',
-    }
+    },
+    # 'DEFAULT_PARSER_CLASSES': [
+    #     'rest_framework.parsers.JSONParser',
+    #     'rest_framework_xml.parsers.XMLParser',
+    #     'rest_framework_yaml.parsers.YAMLParser',
+    # ],
+    # 'DEFAULT_RENDERER_CLASSES': [
+    #     'rest_framework.renderers.JSONRenderer',
+    #     'rest_framework_xml.renderers.XMLRenderer',
+    #     'rest_framework_yaml.renderers.YAMLRenderer',
+    # ],
 }
 
 # CORS
